33897d1229b2028715e8899bc6ce5af457989b22
Chareunm <123622802+Chareunm@users.noreply.github.com>
2023-03-10 16:27:28 +0100

    Add files via upload

---
+++ Prog-Odoo-1-master/odoo/local-src/students/__manifest__.py
@@ -0,0 +1,19 @@
+{
+    "name": "Gestion des étudiants",
+    "version": "0.10",
+    "category": "Generic Modules/Others",
+    "description": """Test création module gestion des étudiants Odoo v14""",
+    "author": "CHAREUN Maximilien",
+    "depends": ["base"],
+    "data": [
+        "data/students_training_data.xml",
+        "views/students_views.xml",
+        "views/training_views.xml",
+        "views/mark_views.xml",
+        "views/menu_views.xml",
+        "demo/students_student_data.xml",
+        "demo/students_mark_data.xml",
+    ],
+    "installable": True,
+    "auto_install": False,
+}
